0f1ad944d6ec9d64988dbece015d718e9b8fcf17
Abdisamadovich <islombobokulov143@gmail.com>
2024-07-24 16:13:55 +0000

    Merged PR 6: Create logging
    Related work items: #5

--- MyCar.Server/Program.cs
+++ MyCar.Server/Program.cs
@@ -3,15 +3,16 @@ using MyCar.DataAccess;
 using MyCar.Repository.Interfaces;
 using MyCar.Repository.Repository;
 using MyCar.Server.Configuration;
+using MyCar.Server.Middlewares;
 using MyCar.Service.Interfaces;
 using MyCar.Service.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add Application Insights
+builder.Services.AddApplicationInsightsTelemetry();
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.ConfigureCorsPolicy();
@@ -26,8 +27,8 @@ builder.Services.AddScoped<IExpenseRepository, ExpenseRepository>();
 builder.Services.AddScoped<IExpenseTypeRepository, ExpenseTypeRepository>();
 builder.Services.AddScoped<IServiceTypeRepository, ServiceTypeRepository>();
 builder.Services.AddScoped<IServiceRepository, ServiceRepository>();
-builder.Services.AddScoped<IExpenseRepository,ExpenseRepository>();
-builder.Services.AddScoped<IExpenseTypeRepository,ExpenseTypeRepository>();
+builder.Services.AddScoped<IExpenseRepository, ExpenseRepository>();
+builder.Services.AddScoped<IExpenseTypeRepository, ExpenseTypeRepository>();
 builder.Services.AddScoped<IRecordRepository, RecordRepository>();
 
 // Service
@@ -42,21 +43,28 @@ builder.Services.AddScoped<IExpenseService, ExpenseService>();
 builder.Services.AddScoped<IExpenseTypeService, ExpenseTypeService>();
 builder.Services.AddScoped<IRecordService, RecordService>();
 
+var connectionString = builder.Configuration.GetConnectionString("MainDatabase");
 builder.Services.AddDbContext<MainContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("MainDatabase"));
+    options.UseSqlServer(connectionString);
 });
 
 var app = builder.Build();
 
+// Get the logger instance
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+logger.LogInformation("Application started.");
+
 app.UseDefaultFiles();
 app.UseStaticFiles();
+app.UseMiddleware<ExceptionHandlerMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    logger.LogInformation("Swagger UI configured.");
 }
 
 app.UseHttpsRedirection();
@@ -64,4 +72,8 @@ app.UseAuthorization();
 app.MapControllers();
 app.UseCors("AllowSpecificOrigin");
 app.MapFallbackToFile("index.html");
+
+logger.LogInformation("HTTP request pipeline configured.");
+
 app.Run();
+logger.LogInformation("Application is running.");
